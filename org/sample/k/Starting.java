403a7719b0cdf4cabb7fa41012368c2a764ff644
kunalksingh <kunalk.singh9097@gmail.com>
2019-07-10 01:20:39 +0530

    new

---
+++ org/sample/k/Starting.java
@@ -0,0 +1,16 @@
+package org.sample.k;
+
+
+
+public class Starting {
+	public static void main(String[] args) {
+ 
+
+		System.out.println("Regression");
+
+	System.out.println("smoke");
+	
+
+
+}
+}
